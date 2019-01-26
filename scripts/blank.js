949f6557ab2eea84e78ccec44cd98157eb1b95cc
Will Wallace <wwallace@siteset.co.uk>
2019-01-26 20:02:43 +0000

    ?

--- scripts/blank.js
+++ scripts/blank.js
@@ -3,9 +3,12 @@
 
 $(function(){	
 	$('a').each(function() {
-	   var a = new RegExp('/' + window.location.host + '/');
-	   if(!a.test(this.href)) {
-	       $(this).attr('target', '_blank');
-	   }
-	});
-})
+		var a = new RegExp('/' + window.location.host + '/');
+		if(!a.test(this.href)) {
+			$(this).attr({
+	    		'target': '_blank',
+	    		'rel': 'noopener'
+	    	})
+		}
+	})
+});
